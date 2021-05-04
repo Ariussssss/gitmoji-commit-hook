f26db34d26a91d290c1febe1b16f81b29da6e608
Ariussssss <1059146811@qq.com>
2021-05-04 22:41:11 +0800

    cache if net error

--- bin/lib.js
+++ bin/lib.js
@@ -1,14 +1,16 @@
-'use strict';
+"use strict";
 
-const { writeFile, readFile, chmod } = require('fs').promises;
-const inquirer = require('inquirer');
-const fetch = require('node-fetch');
-const chalk = require('chalk');
-const pathExists = require('path-exists');
-const fileExists = require('file-exists');
+const { writeFile, readFile, chmod } = require("fs").promises;
+const inquirer = require("inquirer");
+const fetch = require("node-fetch");
+const chalk = require("chalk");
+const pathExists = require("path-exists");
+const fileExists = require("file-exists");
+const { gitmojis: defaultConfig } = require("./gitmojis-default.json");
 
-const gitmojiUrl = 'https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json';
-const prepareCommitMsgFileName = 'prepare-commit-msg';
+const gitmojiUrl =
+  "https://raw.githubusercontent.com/carloscuesta/gitmoji/master/src/data/gitmojis.json";
+const prepareCommitMsgFileName = "prepare-commit-msg";
 
 const gitmojiCommitHookComand = `#!/bin/sh
 exec < /dev/tty
@@ -16,7 +18,7 @@ gitmoji-commit-hook $1
 `;
 
 const errorMessage = {
-  notGit: 'The directory is not a git repository.',
+  notGit: "The directory is not a git repository.",
   commitHookExist: `A prepare-commit hook already exists, please remove the hook (rm .git/hooks/${prepareCommitMsgFileName}) or install gitmoji-commit-hook manually by adding the following content info .git/hooks/\n\n${prepareCommitMsgFileName}:${gitmojiCommitHookComand}`,
   gitmojiParse: `Could not find gitmojis at ${gitmojiUrl}.`
 };
@@ -27,53 +29,62 @@ function errorHandler(error) {
 }
 
 function rejectIf(errorMsg) {
-  return val => val ? Promise.reject(new Error(errorMsg)) : val;
+  return val => (val ? Promise.reject(new Error(errorMsg)) : val);
 }
 
 function rejectIfNot(errorMsg) {
-  return val => val ? val : Promise.reject(new Error(errorMsg));
+  return val => (val ? val : Promise.reject(new Error(errorMsg)));
 }
 
 async function getGitmojiList() {
   try {
     const { gitmojis } = await fetch(gitmojiUrl).then(r => r.json());
     if (!gitmojis) {
-      throw new Error(errorMessage.gitmojiParse);
+      console.error(errorMessage.gitmojiParse);
+      return defaultConfig;
     }
     return gitmojis;
   } catch (error) {
-    throw new Error(errorMessage.gitmojiParse + ` ${error.message}`);
+    console.error(errorMessage.gitmojiParse + ` ${error.message}`);
+    return defaultConfig;
   }
 }
 
 function assertGitRepository() {
-  return pathExists('.git')
-    .then(rejectIfNot(errorMessage.notGit));
+  return pathExists(".git").then(rejectIfNot(errorMessage.notGit));
 }
 
 function assertNoPrepareCommitHook(gitHookPath) {
-  return () => fileExists(`${gitHookPath}/${prepareCommitMsgFileName}`)
-    .then(rejectIf(errorMessage.commitHookExist));
+  return () =>
+    fileExists(`${gitHookPath}/${prepareCommitMsgFileName}`).then(
+      rejectIf(errorMessage.commitHookExist)
+    );
 }
 
 function initProject(gitHookPath) {
   return assertGitRepository()
     .then(assertNoPrepareCommitHook(gitHookPath))
-    .then(() => writeFile(`${gitHookPath}/${prepareCommitMsgFileName}`, gitmojiCommitHookComand))
-    .then(() => chmod(`${gitHookPath}/${prepareCommitMsgFileName}`, '755'));
+    .then(() =>
+      writeFile(
+        `${gitHookPath}/${prepareCommitMsgFileName}`,
+        gitmojiCommitHookComand
+      )
+    )
+    .then(() => chmod(`${gitHookPath}/${prepareCommitMsgFileName}`, "755"));
 }
 
 function prependMessage(getMessage, putMessage) {
-  return filepath => message => getMessage(filepath)
-    .then(fileContent => `${message} ${fileContent}`)
-    .then(fileContent => putMessage(filepath, fileContent));
+  return filepath => message =>
+    getMessage(filepath)
+      .then(fileContent => `${message} ${fileContent}`)
+      .then(fileContent => putMessage(filepath, fileContent));
 }
 
 const prependMessageToFile = prependMessage(readFile, writeFile);
 
 function getGitmojiBlacklist() {
   return fileExists(`${process.env.PWD}/package.json`)
-    .then(exist => exist ? require(`${process.env.PWD}/package.json`) : {})
+    .then(exist => (exist ? require(`${process.env.PWD}/package.json`) : {}))
     .then(packageJson => packageJson.gitmoji || {})
     .then(gitmoji => gitmoji.blacklist || []);
 }
@@ -93,35 +104,41 @@ function seperateChoices(choices) {
 }
 
 function seperateBlacklistEmojis(choices) {
-  return getGitmojiBlacklist()
-    .then(seperateChoices(choices));
+  return getGitmojiBlacklist().then(seperateChoices(choices));
 }
 
 function printInitSuccess() {
-  console.log(`${chalk.green('🎉  SUCCESS 🎉')}  gitmoji-commit-hook initialized with success.`);
+  console.log(
+    `${chalk.green(
+      "🎉  SUCCESS 🎉"
+    )}  gitmoji-commit-hook initialized with success.`
+  );
 }
 
 function mapGitmojiItemToOption(gitmoji) {
   return {
-    name: gitmoji.emoji + '  ' + gitmoji.description,
+    name: gitmoji.emoji + "  " + gitmoji.description,
     value: gitmoji.emoji,
     type: gitmoji.name
   };
 }
 
 function createInquirerQuestion(emojis) {
-  return [{
-    type: 'checkbox',
-    name: 'emoji',
-    message: 'Select emoji(s) for your commit',
-    choices: emojis
-  }];
+  return [
+    {
+      type: "checkbox",
+      name: "emoji",
+      message: "Select emoji(s) for your commit",
+      choices: emojis
+    }
+  ];
 }
 
-const isCommitEditMsgFile = stringToTest => /COMMIT_EDITMSG/g.test(stringToTest);
+const isCommitEditMsgFile = stringToTest =>
+  /COMMIT_EDITMSG/g.test(stringToTest);
 
 function gitmojiCommitHook(gitHookPath, commitFile) {
-  if (commitFile === '--init') {
+  if (commitFile === "--init") {
     initProject(gitHookPath)
       .then(printInitSuccess)
       .catch(errorHandler);
@@ -132,7 +149,7 @@ function gitmojiCommitHook(gitHookPath, commitFile) {
       .then(createInquirerQuestion)
       .then(inquirer.prompt)
       .then(answers => answers.emoji)
-      .then(answers => answers.join(' '))
+      .then(answers => answers.join(" "))
       .then(prependMessageToFile(commitFile))
       .catch(errorHandler);
   }
